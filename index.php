ce3cde08d1d92354b2737453561fd2ad3670d0b0
yegarcia99 <yegarcia9910@gmail.com>
2026-05-15 18:37:29 -0500

    15/05/2026

--- index.php
+++ index.php
@@ -145,7 +145,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         </form>
 
         <div class="login-footer">
-            SENA &bull; Centro de Comercio y Servicios &bull; <?= date('Y') ?>
+            SENA &bull; Regional Caldas &bull; <?= date('Y') ?>
         </div>
     </div>
 </div>
